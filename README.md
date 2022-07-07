337b416070203cba7d4c6cefc0eccb18292a3e1e
Eduardo Fischer <efischer@outlook.com.br>
2022-07-07 08:21:03 -0300

    limpeza e organização dos scripts de processamento de dados

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A tool for visualization of Brazilian colaboration networks based on the Sucupir
 
 ## Data Processing
 
-Install the Python3 requirements:
+- Download the `datasets` directory [here](https://infufrgsbr-my.sharepoint.com/:f:/g/personal/esfischer_inf_ufrgs_br/Esnbvcy5TtxBmFiWG9IQt1oBYyfkXbYrT5yyZR6hr3I6eA?e=oEJHtN) and put it in `data_processing/`.
+
+- Install Python3 requirements:
 
 `python3 -m pip install -r requirements.txt`
---
+++ data_processing/utils.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import re, unicodedata
+from collections import Counter
+
+# Data Type Optimization
+# https://medium.com/bigdatarepublic/advanced-pandas-optimize-speed-and-memory-a654b53be6c2
+
+def optimize_floats(df: pd.DataFrame) -> pd.DataFrame:
+    floats = df.select_dtypes(include=['float64']).columns.tolist()
+    df[floats] = df[floats].fillna(0).astype('int64')
+    return df
+
+def optimize_ints(df: pd.DataFrame) -> pd.DataFrame:
+    ints = df.select_dtypes(include=['int64']).columns.tolist()
+    df[ints] = df[ints].apply(pd.to_numeric, downcast='integer')
+    return df
+
+def optimize_objects(df: pd.DataFrame) -> pd.DataFrame:
+    for col in df.select_dtypes(include=['object']):
+        if not (type(df[col][0])==list):
+            num_unique_values = len(df[col].unique())
+            num_total_values = len(df[col])
+            if float(num_unique_values) / num_total_values < 0.5:
+                df[col] = df[col].astype('category')
+    return df
+
+def optimize(df: pd.DataFrame):
+    return optimize_ints(optimize_floats(optimize_objects(df)))
+
+# Data Normalization
+# https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-normalize-in-a-python-unicode-string
+def strip_accents(text):
+  text = unicodedata.normalize('NFD', text)
+  text = text.encode('ascii', 'ignore')
+  text = text.decode("utf-8")
+  return str(text)
+
+def normalize_name(name):
+  if ',' not in name:
+    norm = name
+  else:
+    it = name.split(', ')
+    it.reverse()
+    norm = ' '.join(it)
+
+  # remove accents
+  norm = strip_accents(norm)
+
+  # remove invalid chars
+  norm = re.sub('[_-]', ' ', norm)
+  norm = re.sub('[0-9?&#;()]', '', norm)
+
+  # remove leading and trailing spaces
+  norm = norm.strip()
+
+  return norm
+
+def firstAndLastName(name):
+  it = name.split(' ')
+  return ' '.join([it[0], it[-1]])
+
+
+# Aggregation Funtions
+def agg_to_dict(items):
+  return items.astype(str).value_counts().to_dict()
+
+def most_frequent(items):
+  values = items.dropna()
+  if len(values) == 0: return None
+  occurence_count = Counter(values)
+  return occurence_count.most_common(1)[0][0]
+
+def toArray(item):
+  return item if hasattr(item, '__iter__') and not isinstance(item, str) else [item]
+
+def priority(priority_list):
+  return lambda items: next((type for type in priority_list if type in items.array), None)
