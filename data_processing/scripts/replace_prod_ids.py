b682dbcf6f989ba7518514974677066566fd2d7e
Eduardo Fischer <efischer@outlook.com.br>
2022-08-18 01:23:41 -0300

    [#21] refatoração do processamento de autores, com fusão do grouping e normalization em um único notebook, exportação de dataset parcial e geração de arquivo CSV com dados de co-autorias

--- data_processing/scripts/replace_prod_ids.py
+++ data_processing/scripts/replace_prod_ids.py
@@ -1,14 +1,31 @@
+import csv
 import json
 import pandas as pd
 from ast import literal_eval
+from collections import defaultdict
 
-authors = pd.read_csv('output/normalized_authors.csv', delimiter=";", index_col="IDX")
+authors = pd.read_csv('output/processed_authors_preliminary.csv', delimiter=";", index_col="IDX")
 
 authors['ID_ADD_PRODUCAO_INTELECTUAL'] = authors['ID_ADD_PRODUCAO_INTELECTUAL'].apply(literal_eval)
 authors = authors.explode('ID_ADD_PRODUCAO_INTELECTUAL')
+authors.reset_index()
 
 replacements = json.load(open('output/prod_id_replacements.json', 'r'))
 authors['ID_ADD_PRODUCAO_INTELECTUAL'] = authors['ID_ADD_PRODUCAO_INTELECTUAL'].map(replacements).fillna(authors['ID_ADD_PRODUCAO_INTELECTUAL'])
 
 authors.to_csv('output/final_authors.csv', sep=';')
 
+# Get list of authors for each production
+authors_per_prod = defaultdict(set)
+for idx, row in authors.iterrows():
+  authors_per_prod[row['ID_ADD_PRODUCAO_INTELECTUAL']].add(idx)
+
+# Write CSV file with co-authorships data
+with open('../output/co_authorships.csv', 'w') as f:
+  writer = csv.writer(f, delimiter=';')
+  writer.writerow([ 'AUTHOR_1', 'AUTHOR_2', 'PROD_ID' ])
+  for prod_id in authors_per_prod:
+    authors = list(authors_per_prod[prod_id])
+    for i in range(len(authors)):
+      for j in range(i+1, len(authors)):
+        writer.writerow([authors[i], authors[j], prod_id])
--- data_processing/utils.py
+++ data_processing/utils.py
@@ -1,7 +1,7 @@
+import os
 import pandas as pd
 import re, unicodedata
 from ast import literal_eval
-from collections import Counter
 
 # Data Type Optimization
 # https://medium.com/bigdatarepublic/advanced-pandas-optimize-speed-and-memory-a654b53be6c2
@@ -28,7 +28,7 @@ def optimize_objects(df: pd.DataFrame) -> pd.DataFrame:
 def optimize(df: pd.DataFrame):
     return optimize_ints(optimize_floats(optimize_objects(df)))
 
-# Data Normalization
+## Data Normalization
 # https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-normalize-in-a-python-unicode-string
 
 def strip_accents(text):
@@ -78,16 +78,154 @@ def firstAndLastName(name):
   it = name.split(' ')
   return ' '.join([it[0], it[-1]])
 
-# Aggregation Funtions
+## Aggregation Funtions
 
 def count_to_dict(items):
-  return items.value_counts().to_dict()
+  try:
+    if type(items.iloc[0]) is dict:
+      item_count = {}
+      for _, item in items.iteritems():
+        merge_count_dicts(item_count, item)
+      return item_count
+    else:
+      return items.value_counts().to_dict()
+  except Exception as e:
+    print(e)
 
-def most_frequent(items):
-  values = items.dropna()
-  if len(values) == 0: return None
-  occurence_count = Counter(values)
-  return occurence_count.most_common(1)[0][0]
+def merge_count_dicts(item_count, other_item_count):
+  try:
+    # assign empty dict to nan values
+    if type(item_count) is not dict:
+      item_count = {}
+    if type(other_item_count) is not dict:
+      other_item_count = {}
+      
+    # merge dicts
+    for key in other_item_count:
+      if key in item_count:
+        item_count[key] += other_item_count[key]
+      else:
+        item_count[key] = other_item_count[key]
+  except Exception as e:
+    print('Error merging item counts: {}'.format(e))
+    print(item_count)
+    print(other_item_count)
+
+def most_freq(d):
+  try:
+    x = -1
+    k = None
+    for key in d:
+      if (d[key] > x and key != 'nan'):
+        x = d[key]
+        k = key
+  except:
+    return '-'
+
+  return k if k != 'nan' else '-'
+
+def select_by_priority(priority_list, d):
+  try:
+    n = next((type for type in priority_list if type in d.keys()), None)
+  except:
+    n = None
+  return n 
 
 def priority(priority_list):
-  return lambda items: next((type for type in priority_list if type in items.array), None)
+  return lambda d: select_by_priority(priority_list, d)
+
+## Merging Functions
+
+# Compare two authors and return a value `n` indicating the probability that both authors are the same person
+def compare_authors(author, orphan):
+  n = 0
+
+  try:
+    # Match first and last name
+    for name in orphan['FIRST_LAST_NAME']:
+      if name in author['FIRST_LAST_NAME']: n += 2;
+
+    # Return if there's no chance of match
+    if n == 0: return 0
+
+    # Match abnt name
+    if type(author['NM_ABNT_AUTOR']) is dict and type(orphan['NM_ABNT_AUTOR']) is dict:
+      for abnt in orphan['NM_ABNT_AUTOR']:
+        if abnt in author['NM_ABNT_AUTOR']: n += 1;
+    # Match university
+    if type(author['SG_ENTIDADE_ENSINO']) is dict and type(orphan['SG_ENTIDADE_ENSINO']) is dict:
+      for university in orphan['SG_ENTIDADE_ENSINO']:
+        if university in author['SG_ENTIDADE_ENSINO']: n += 1;
+    # Match author type
+    if type(author['TP_AUTOR']) is dict and type(orphan['TP_AUTOR']) is dict:
+      for author_type in orphan['TP_AUTOR']:
+        if author_type in author['TP_AUTOR'] and author_type != '-': n += 1;
+    # Match IES program
+    if type(author['NM_PROGRAMA_IES']) is dict and type(orphan['NM_PROGRAMA_IES']) is dict:
+      for ies_program in orphan['NM_PROGRAMA_IES']:
+        if ies_program in author['NM_PROGRAMA_IES']: n += 1;
+
+    return n
+  except Exception as e:
+    print("Error comparing authors: {}".format(e))
+    return 0
+
+def merge_authors(author, orphan_author, merge_schema):
+  merged = author.copy(deep=True)
+  for column in author.index.to_list():
+    if column not in merge_schema or column == 'FULL_NAME': continue
+    author_value = author[column]
+    orphan_value = orphan_author[column]
+    if isinstance(author_value, list):
+      author_value.append(orphan_value)
+    else:
+      merge_count_dicts(author_value, orphan_value)
+
+  return merged
+
+## Exporting functions
+
+def format_authors_dataframe(authors):
+  dict_fields = [
+    'NM_AUTOR',
+    'NM_ABNT_AUTOR',
+    'CD_PROGRAMA_IES',
+    'NM_PROGRAMA_IES',
+    'NM_AREA_CONHECIMENTO',
+    'SG_ENTIDADE_ENSINO',
+  ]
+
+  # Drop helper columns
+  authors = authors.drop(columns=['FULL_NAME', 'FIRST_LAST_NAME'])
+
+  authors[['TP_AUTOR']] = authors[['TP_AUTOR']].applymap(priority(['DOCENTE', 'EGRESSO', 'PÓS-DOC', 'DISCENTE', 'PARTICIPANTE EXTERNO']))
+  authors[['NM_TP_CATEGORIA_DOCENTE']] = authors[['NM_TP_CATEGORIA_DOCENTE']].applymap(priority(['PERMANENTE', 'COLABORADOR', 'VISITANTE']))
+  authors[['NM_NIVEL_DISCENTE']] = authors[['NM_NIVEL_DISCENTE']].applymap(priority(['DOUTORADO PROFISSIONAL', 'BACHARELADO', 'MESTRADO', 'DOUTORADO', 'MESTRADO PROFISSIONAL']))
+  authors[dict_fields] = authors[dict_fields].applymap(most_freq)
+
+  # Add productions count to each author
+  authors['PROD_COUNT'] = authors[['ID_ADD_PRODUCAO_INTELECTUAL']].applymap(len)
+
+  # Map nan author types to 'NÃO INFORMADO'
+  authors['TP_AUTOR'] = authors['TP_AUTOR'].fillna('NÃO INFORMADO')
+
+  return authors
+
+def export_authors_dataframe(df, filename):
+  authors = format_authors_dataframe(df.copy())
+  path = 'output/{}'.format(filename)
+
+  print('⚙️ Exporting authors to {}...'.format(path))
+  authors.index.name = 'IDX'
+  os.makedirs('output/', exist_ok=True)
+  authors.to_csv(path, sep=';')
+
+## Other
+
+def parse_array(s):
+  try:
+    lit = literal_eval(s)  
+  except:
+    return []
+
+  return lit
